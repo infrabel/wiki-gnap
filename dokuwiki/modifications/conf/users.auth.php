521d62a38b831e8c149daec4c63786d6d33f8872
Infrabel <dotnet@infrabel.be>
2014-06-18 23:02:18 +0200

    Add default gnap user

---
+++ dokuwiki/modifications/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+gnap:$1$4fd0ad31$.cId7p1uxI4a.RcrH81On0:GNaP:dotnet@infrabel.be:admin,user
