c019a99fd395fc5430b1c1d76115040747c7b636
Infrabel <dotnet@infrabel.be>
2014-06-24 20:36:32 +0200

    Added listeabo

---
+++ dokuwiki/modifications/lib/plugins/listeabo/GNaP.Modifications.md
@@ -0,0 +1,4 @@
+lang/en/lang.php - Changed wording to better integrate into the whole wiki
+action.php - Made it to work with the $nom explode
+action.php - Changed the titles
+action.php - Print out less noise
---
+++ dokuwiki/modifications/lib/plugins/listeabo/lang/en/lang.php
@@ -0,0 +1,16 @@
+<?php
+$lang['menu']                  = 'Subscriptions';
+$lang['abo_aboactifs']         = 'Current subscriptions';
+$lang['abo_abode']             = 'Subscriptions for';
+$lang['abo_noabo']             = 'You currently have no subscriptions';
+$lang['abo_estadmin']          = 'You are administrator, you can ';
+$lang['abo_voirtousabo']       = 'see all subscriptions';
+$lang['abo_voirvosabo']        = 'See your subscriptions';
+$lang['abo_whoto']             = '';//'To subscribe to a page or namespace, click the green cloud icon on top and select "Manage Subscriptions".<br/>You will receive a e-mail each time this page or any page within the namespace is modified.';
+$lang['abo_cat']               = 'Category';
+$lang['abo_cptabo']            = 'subscription(s)';
+$lang['abo_cptdans']           = 'on';
+$lang['abo_cptpages']          = 'page(s)';
+$lang['abo_cptet']             = 'and';
+$lang['abo_cptcat']            = 'category(ies)';
+
